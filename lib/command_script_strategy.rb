dfb49d52905f2d71b0ab3029979eafb9aac6fe73
Pearl Zeng <zhuzeng@umich.edu>
2019-06-08 20:29:48 -0400

    Fix CommandScriptStrategy to give an error message when an exit function is not added to the test

--- lib/command_script_strategy.rb
+++ lib/command_script_strategy.rb
@@ -5,6 +5,10 @@ class CommandScriptStrategy
   end
 
   def prompt(key)
+    if !@command_script_items[@current_index]
+      raise "A prompt was given but all the command script items have already been used. Please put in an exit command."
+    end
+
     @command_script_items[@current_index][key].tap do
       @current_index += 1
     end
--- spec/controllers/providers_controller_spec.rb
+++ spec/controllers/providers_controller_spec.rb
@@ -3,19 +3,44 @@ require_relative '../../lib/interface'
 require_relative '../../lib/command_script_strategy'
 
 RSpec.describe ProvidersController do
-  it 'adds a provider to the list' do
-    $all_providers = []
+  describe '#add' do
+    it 'adds a provider to the list' do
+      $all_providers = []
 
-    command_script_strategy = CommandScriptStrategy.new([
-      { 'Please enter a command:' => 'sp:add' },
-      { 'Provider Name:' => 'Jason' },
-      { 'Provider Phone Number:' => '555' },
-      { 'Please enter a command:' => 'exit' },
-   ])
+      command_script_strategy = CommandScriptStrategy.new([
+        { 'Please enter a command:' => 'sp:add' },
+        { 'Provider Name:' => 'Jason' },
+        { 'Provider Phone Number:' => '555' },
+        { 'Please enter a command:' => 'exit' },
+      ])
 
-    interface = Interface.new(command_script_strategy)
-    interface.start
+      interface = Interface.new(command_script_strategy)
+      interface.start
 
-    expect(ProviderHelper.new.find_provider_by_name('Jason').name).to eq ('Jason')
+      expect(ProviderHelper.new.find_provider_by_name('Jason').name).to eq ('Jason')
+    end
+  end
+
+  describe '#remove' do
+    it 'removes a provider from the list' do
+      $all_providers = [
+        Provider.new('Jason', '1111111111', [Service.new('A', 200, 60)], [], []), 
+        Provider.new('Yasmine', '1111111111', [Service.new('A', 200, 60)], [], []), 
+        Provider.new('Kai', '1111111111', [Service.new('A', 200, 60)], [], [])
+      ]
+
+      command_script_strategy = CommandScriptStrategy.new([
+        { 'Please enter a command:' => 'sp:remove' },
+        { 'Provider Name To Remove:' => 'Kai' },
+        {'(y/n):' => 'y'},
+        { 'Please enter a command:' => 'exit' }
+      ])
+
+      interface = Interface.new(command_script_strategy)
+      interface.start
+
+      expect(ProviderHelper.new.find_provider_by_name('Yasmine').name).to eq ('Yasmine')
+      expect(ProviderHelper.new.find_provider_by_name('Kai')).to be (false)
+    end
   end
 end
