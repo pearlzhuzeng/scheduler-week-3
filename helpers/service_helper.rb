ab5453062ee83800d07d07440ff256c2449fab6f
Pearl Zeng <zhuzeng@umich.edu>
2019-06-08 20:26:51 -0400

    Regroup helper functions

--- helpers/service_helper.rb
+++ helpers/service_helper.rb
@@ -1,9 +1,12 @@
+require_relative './utility_helper'
+require_relative '../models/provider'
+
 class ServiceHelper
   def error_message
     puts ''
     puts 'Service Provider Not Found...'
     puts 'Choose from the following:'
-    spPrint($all_providers)
+    utilityHelper.new.print_providers(Provider.all)
   end
 end
 
--- helpers/utility_helper.rb
+++ helpers/utility_helper.rb
@@ -1,3 +1,6 @@
+require_relative '../vendors/tty/colors'
+require_relative '../lib/prompt_input_strategy'
+
 class UtilityHelper
   def notify_success
     puts 'Success!'
@@ -6,7 +9,7 @@ class UtilityHelper
 
   def yes_or_no
     loop do
-      y_n = $prompt.ask('(y/n):')
+      y_n = PromptInputStrategy.new.ask('(y/n):')
       if y_n == 'y'
         return true
       elsif y_n == 'n'
@@ -15,5 +18,25 @@ class UtilityHelper
         puts "Enter y or n"
       end
     end
+  end
+
+  def print_providers(providers)
+    puts "#{BgMagenta}SERVICE PROVIDERS:#{Reset}"
+    puts '------------------'
+    providers.each do |provider|
+      puts "#{Magenta}#{provider.name}#{Reset}"
+    end
+    puts '------------------'
+    puts ''
+  end
+  
+  def print_provider_services(providers)
+    puts "#{BgCyan}SERVICES:#{Reset}"
+    puts '------------------'
+    for provider in providers do
+      provider.print_services
+    end
+    puts '------------------'
+    puts ''
   end
 end
