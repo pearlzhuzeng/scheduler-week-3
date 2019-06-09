4e899a829059336b71717e7919556969c1abf1d1
Pearl Zeng <zhuzeng@umich.edu>
2019-06-08 20:26:18 -0400

    Rewrite seed data using our data model objects

---
+++ seed.rb
@@ -0,0 +1,19 @@
+require_relative './models/service'
+require_relative './models/provider'
+
+class Service
+  @services = [
+    Service.new('Mind Reading', 200, 60),
+    Service.new('Demonic Exorcism', 50, 120),
+    Service.new('Potion Therapy', 100, 100),
+    Service.new('Liver Transplants', 5000, 600),
+  ]
+end
+
+class Provider
+  @providers = [
+    Provider.new('Jim', '234-486-9800', Service.all, [], []),
+    Provider.new('Sue', '978-123-5768', Service.all[0..2], [], [])
+  ]
+end
+
