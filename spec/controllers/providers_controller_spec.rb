5b50156ba576a6a7e16e930674e4074f4836c905
Pearl Zeng <zhuzeng@umich.edu>
2019-06-11 15:57:30 -0400

    Make provider_controller_spec #add and #remove pass

--- spec/controllers/providers_controller_spec.rb
+++ spec/controllers/providers_controller_spec.rb
@@ -1,3 +1,4 @@
+require_relative '../../seed'
 require_relative '../../controllers/providers_controller'
 require_relative '../../lib/interface'
 require_relative '../../lib/command_script_strategy'
@@ -5,42 +6,28 @@ require_relative '../../lib/command_script_strategy'
 RSpec.describe ProvidersController do
   describe '#add' do
     it 'adds a provider to the list' do
-      $all_providers = []
-
       command_script_strategy = CommandScriptStrategy.new([
-        { 'Please enter a command:' => 'sp:add' },
         { 'Provider Name:' => 'Jason' },
-        { 'Provider Phone Number:' => '555' },
-        { 'Please enter a command:' => 'exit' },
+        { 'Provider Phone Number:' => '5558889981' },
       ])
 
-      interface = Interface.new(command_script_strategy)
-      interface.start
+      ProvidersController.add(command_script_strategy)
 
-      expect(ProviderHelper.new.find_provider_by_name('Jason').name).to eq ('Jason')
+      expect(Provider.find_provider_by_name('Jason').name).to eq ('Jason')
     end
   end
 
   describe '#remove' do
     it 'removes a provider from the list' do
-      $all_providers = [
-        Provider.new('Jason', '1111111111', [Service.new('A', 200, 60)], [], []), 
-        Provider.new('Yasmine', '1111111111', [Service.new('A', 200, 60)], [], []), 
-        Provider.new('Kai', '1111111111', [Service.new('A', 200, 60)], [], [])
-      ]
-
       command_script_strategy = CommandScriptStrategy.new([
-        { 'Please enter a command:' => 'sp:remove' },
-        { 'Provider Name To Remove:' => 'Kai' },
+        { 'Provider Name To Remove:' => 'Sue' },
         {'(y/n):' => 'y'},
-        { 'Please enter a command:' => 'exit' }
       ])
 
-      interface = Interface.new(command_script_strategy)
-      interface.start
-
-      expect(ProviderHelper.new.find_provider_by_name('Yasmine').name).to eq ('Yasmine')
-      expect(ProviderHelper.new.find_provider_by_name('Kai')).to be (false)
+      ProvidersController.remove(command_script_strategy)
+      
+      expect(Provider.find_provider_by_name('Jim').name).to eq ('Jim')
+      expect(Provider.find_provider_by_name('Sue')).to be (false)
     end
   end
 end
