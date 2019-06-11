44453cf2377fcf534d76f552404000d119a9d9f1
Pearl Zeng <zhuzeng@umich.edu>
2019-06-11 17:14:41 -0400

    Allow adding availability to current provider or from provider_name input
    Refactor AvailabilitiesController#add to have a separate function
     #add_to_provider that takes in a provider_name
    
    Co-authored-by: yfrigui <yasmine.frigui@berkeley.edu>

--- controllers/availabilities_controller.rb
+++ controllers/availabilities_controller.rb
@@ -1,26 +1,31 @@
+require 'date'
 require_relative '../lib/prompt_input_strategy'
 
 class AvailabilitiesController
   def self.add(*args)
     provider_name = PromptInputStrategy.new.ask('Provider Name:')
+    add_to_provider(provider_name)
+  end
+
+  def self.add_to_provider(provider_name)
     month = PromptInputStrategy.new.ask('Date (MM):')
     day = PromptInputStrategy.new.ask('Date (DD):')
     year = PromptInputStrategy.new.ask('Date (YYYY):')
     start_time = PromptInputStrategy.new.ask('Start Time (ex: 13:30):')
     end_time = PromptInputStrategy.new.ask('End Time (ex: 14:30):')
-    start_temp = start_time.split(':')
-    start_hour = start_temp[0].to_i
-    start_minute = start_temp[1].to_i
-
-    end_temp = end_time.split(':')
-    end_hour = end_temp[0].to_i
-    end_minute = end_temp[1].to_i
+    start_datetime = format_datetime(month, day, year, start_time)
+    end_datetime = format_datetime(month, day, year, end_time)
 
     provider = Provider.find_provider_by_name(provider_name)
-
-    start_datetime = DateTime.new(year.to_i, month.to_i, day.to_i, start_hour, start_minute)
-    end_datetime = DateTime.new(year.to_i, month.to_i, day.to_i, end_hour, end_minute)
-
     provider.add_availability(start_datetime, end_datetime)
+  end
+
+  private
+
+  def self.format_datetime(month, day, year, time)
+    hour_and_minute = time.split(':')
+    hour = hour_and_minute[0]
+    minute = hour_and_minute[1]
+    DateTime.new(year.to_i, month.to_i, day.to_i, hour.to_i, minute.to_i)
   end
 end
