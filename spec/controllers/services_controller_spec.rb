954768dc64af4414c99a081cb32601c25bca4e65
Shawki <saatassi@umich.edu>
2019-06-12 22:09:22 -0400

    services_controller tests
    test 1 fails because we aren't keeping a master list of services

--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -1,5 +1,46 @@
-require '../controller/services_controllers'
+require_relative '../../seed'
+require_relative '../../controllers/services_controller'
+require_relative '../../lib/interface'
+require_relative '../../lib/command_script_strategy'
 
 RSpec.describe ServicesController do
+  describe '#add' do
+    it 'adds a service to the services list' do
+      $input_strategy = CommandScriptStrategy.new([
+                                                      { 'Service Name:' => 'Fortune Telling' },
+                                                      { 'Service Price:' => '100' },
+                                                      {'Service Length (Mins):' => '90'},
+                                                      {"Add to which provider?:" => 'Sue'},
+                                                  ])
 
+      ServicesController.add
+      expect(Service.find_service_by_name('Fortune Telling').name).to eq ('Fortune Telling')
+    end
+
+    it 'adds a service to a provider' do
+      $input_strategy = CommandScriptStrategy.new([
+                                                      { 'Service Name:' => 'Magic Tricks' },
+                                                      { 'Service Price:' => '100' },
+                                                      {'Service Length (Mins):' => '90'},
+                                                      {"Add to which provider?:" => 'Sue'},
+                                                  ])
+
+      ServicesController.add
+      expect(Provider.find_provider_by_service('Magic Tricks').length).to eq (1)
+      expect(Provider.find_provider_by_service('Magic Tricks').first.name).to eq ('Sue')
+    end
+  end
+
+  describe '#remove' do
+    it 'removes a service from a provider' do
+      $input_strategy = CommandScriptStrategy.new([
+                                                      { 'Service Name:' => 'Fortune Telling' },
+                                                      {'Service Provider:' => 'Sue'},
+                                                  ])
+
+      ServicesController.remove
+
+      expect(Provider.find_provider_by_name('Sue').contains_service?('Fortune Telling')).to be(false)
+    end
+  end
 end
